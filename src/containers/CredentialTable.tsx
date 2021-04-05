23787a0f08dc4d21fe33558170739b410d3a01ba
Sridharan Jayabal <sridharan@docon.co.in>
2021-04-05 20:26:05 +0530

    ui changes for table

--- src/containers/CredentialTable.tsx
+++ src/containers/CredentialTable.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Table } from "react-bootstrap";
 import { useAuthentication } from "./Authentication";
 import { useGlobalTokenValue } from "./MessageListener";
+import CheckCircle from '../assets/images/icons/check_green_circle.svg'
+import CrossCircle from '../assets/images/icons/cross_red_circle.svg'
 
 const CredentialTable = () => {
 
@@ -49,7 +51,10 @@ const CredentialTable = () => {
                     <tr>
                     <th scope="row">{index+1}</th>
                     <td>{data.suppliedCredentials[0].credentialSubject.data.givenName}</td>
-                    <td>{data.isValid ? <p> True </p> : <p> False </p>}</td>
+                    <td>{data.isValid ? <img src={CheckCircle} style={{height: '28px'}} /> : 
+                        <img src={CrossCircle} style={{height: '28px'}} />
+                    }
+                    </td>
                   </tr>
                   )
                 })}
